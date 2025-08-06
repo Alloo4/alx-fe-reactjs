84226f99643fe33159f38ca7f29f7c0acdc3a9d5
ALX Student <alx@student.com>
2025-08-06 09:11:23 -0400

    Add Tailwind React Integration project with styled UserProfile component featuring responsive design and interactive hover effects

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from './components/UserProfile'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <UserProfile />
+    </div>
+  )
+}
+
+export default App
